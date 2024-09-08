8133baebada263b747c43cb6323db2b305709cc2
burhan1997 <burhanelaldi97@gmail.com>
2024-09-08 21:36:38 +0200

    globalstyles component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,13 +2,17 @@ import React, { ReactElement } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 import Home from 'components/Home'
+import GlobalStyles from 'components/GlobalStyles'
 
 const App = (): ReactElement => (
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Home />} />
-		</Routes>
-	</BrowserRouter>
+	<>
+		<GlobalStyles />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	</>
 )
 
 export default App
