b10fe0f9bf639579a9809a9fb49e93aa483400d9
burhan1997 <burhanelaldi97@gmail.com>
2024-09-09 16:14:08 +0200

    style list view component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,10 @@
 import React, { ReactElement } from 'react'
 import GameList from 'components/GameList'
 import withErrorBoundary from 'hoc/withErrorBoundary'
-const Home = (): ReactElement => <GameList />
+const Home = (): ReactElement => (
+	<main>
+		<GameList />
+	</main>
+)
 
 export default withErrorBoundary(Home)
